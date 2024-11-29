530c37f26c64ce878f065f64c0d1ef31a7ee2c30
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2024-11-29 13:47:41 +0100

    Fix location section layout and map display
    - Remove md:ml-auto to eliminate excessive spacing between map and text
    - Add section centering using justify-center and items-center
    - Fix map display by adding fixed height and proper styles
    - Add positioning styles for map iframe
    
    Changes improve responsiveness and aesthetics of location sections.

---
+++ data/locations.js
@@ -0,0 +1,39 @@
+export const locations = [
+  {
+    id: "Euromedis",
+    title: "Centrum Medyczne EuroMedis",
+    hours: "Poniedziałek 10:00 - 16:30 <br/> Czwartek 08:00 - 13:00",
+    address: "al. Powstańców Wielkopolskich 33a, 70-111 Szczecin",
+    phone: "+48 91 818 21 41",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2378.3090876712013!2d14.526484076970622!3d53.40929817001124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47aa091ba999b713%3A0xf86f70dc2d500f0!2sCentrum%20Medyczne%20EuroMedis!5e0!3m2!1spl!2spl!4v1732202800066!5m2!1spl!2spl",
+  },
+  {
+    id: "Fizjoline",
+    title: "Fizjoline - Centrum Medyczne",
+    hours: "1 i 3 Piątek miesiąca <br/> 08:00 - 15:00",
+    address: "Kosynierów 14, 70-786 Szczecin",
+    phone: "+48 91 823 83 58",
+    mapUrl:
+      "//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2380.2524772868346!2d14.6631193!3d53.37453190000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4700a7c90767bd6b%3A0xb26d54e07d669be3!2sFizjoline%20-%20Centrum%20Medyczne%2C%20Rehabilitacji%20i%20Treningu%20Personalnego!5e0!3m2!1spl!2spl!4v1732371826735!5m2!1spl!2spl",
+  },
+  {
+    id: "MAMMA",
+    title: "Gabinety MAMMA",
+    hours: "2 i 4 Piątek miesiąca <br/>08:00 - 14:00",
+    address: "Batalionów Chłopskich 88, 70-764 Szczecin",
+    phone: "+48 91 462 80 00",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2379.8737835181455!2d14.627061376969207!3d53.381307772106766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4700a647bba43733%3A0xfa7c29b202846e1b!2sGabinety%20MAMMA!5e0!3m2!1spl!2spl!4v1732371882670!5m2!1spl!2spl",
+  },
+  {
+    id: "Akademia-Malucha",
+    title: "Akademia Malucha",
+    hours: "Wtorek <br/> 08:00 - 13:00",
+    address: "Niepodległości 1, 72-100 Goleniów",
+    phone: "+48 531 934 855",
+    email: "rejestracja@bieleninikneurologdzieciecy.pl",
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37908.805506518984!2d14.745154380798345!3d53.570256146429806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47009f5ddbe04add%3A0x8c4571377bd2cccb!2sAkademia%20Malucha%20-%20Centrum%20Diagnozy%20i%20Terapii%20Dzieci!5e0!3m2!1spl!2spl!4v1732371943415!5m2!1spl!2spl",
+  },
+];
